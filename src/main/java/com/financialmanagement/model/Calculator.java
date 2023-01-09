ac0dedd2222d8a6ae34565e48818543c80b2a55f
hussain-ghani <hussain.ghani@tlglcohort.com>
2023-01-09 18:21:25 -0500

    Reviewed Classes and team assignments

--- src/main/java/com/financialmanagement/model/Calculator.java
+++ src/main/java/com/financialmanagement/model/Calculator.java
@@ -3,11 +3,11 @@ package com.financialmanagement.model;
 public class Calculator {
 
   // TODO: 1/9/2023 Create ctor with user data (income, expenses)
-  // TODO: 1/9/2023 Create needs, wants and savings methods (Is this how we should separate
-  //  the categories? for the UserDatView
+  // TODO: 1/9/2023 Create needs, wants and savings methods
   // TODO: 1/9/2023 Sum appropriate categories to return their total number and % of income
-  // TODO: 1/9/2023 Generate a total debt/expense to income ratio
+  // TODO: 1/9/2023 Generate a total debt/expense to income ratio(Optional)
   // TODO: 1/9/2023 Store final calculated data in map along with raw data.
+  // TODO: 1/9/23 Create method for user selected budgettype(high, med low) Retrieve user Income and apply rules for calculating budgettype enum
 
 
 
--- src/main/java/com/financialmanagement/view/UserDataView.java
+++
@@ -1,5 +0,0 @@
-package com.financialmanagement.view;
-
-public class UserDataView {
-    // TODO: 1/9/2023 Implement methods from UserData
-}
