43eaa1dfa13a2ea67da0b43ebbafdacd3d24f3d3
dfalfaro <diego.alfaro@tlgcohort.com>
2023-01-11 16:10:08 -0500

    Added changes to classes.

--- src/main/java/com/financialmanagement/view/MainView.java
+++ src/main/java/com/financialmanagement/view/MainView.java
@@ -1,22 +1,52 @@
 package com.financialmanagement.view;
 
+import com.financialmanagement.model.Budget;
+import com.financialmanagement.model.BudgetComparison;
 import com.financialmanagement.model.Category;
 import com.financialmanagement.model.SavingsTarget;
 import java.util.Map;
 
 public class MainView {
 
-    public void showRemainingBudget(double remainingBudget) {
-        System.out.println("Your remaining budget is: $" + remainingBudget);
+    private Budget budget;
+    private BudgetComparison comparison;
+    private SavingsTarget savingsTarget;
+
+    public MainView(Budget budget, SavingsTarget savingsTarget) {
+        this.budget = budget;
+        this.savingsTarget = savingsTarget;
     }
-    public void showBudgetComparison(Map<Category, String> comparisonResult) {
-        for (Map.Entry<Category, String> entry : comparisonResult.entrySet()) {
-            System.out.println(entry.getKey() + " expenses are " + entry.getValue() + " than US average.");
+
+    public void displayIncome() {
+        System.out.println("Your monthly income is: $" + budget.getIncome());
+    }
+
+    public void displayExpenses() {
+        System.out.println("Your monthly expenses are:");
+        for (Category category : Category.values()) {
+            System.out.println("\t" + category + ": $" + budget.getTotalExpenses());
         }
     }
-    public void showSavingsTarget(SavingsTarget savingsTarget) {
-        System.out.println("You've set a savings goal of $" + savingsTarget.getAmount() + " for "
-            + savingsTarget.getType() + " fund, from "+ savingsTarget.getStartDate() + " to "+ savingsTarget.getDeadline() );
+
+    // display comaparison
+    public void displayExpensesComparedToUSAverage() {
+        for (Category category : Category.values()) {
+            double ratio = comparison.compareExpense(category);
+            System.out.printf("Your %s expenses are %.2f% of the US average%n", category, ratio * 100);
+        }
     }
+
+    public void displaySavings() {
+        System.out.println("Your total savings is: $" + budget.getSavings());
+    }
+
+    public void displaySavingsTarget() {
+        System.out.println("Your savings goal is: $" + savingsTarget.getTargetAmount() + " for "
+        + savingsTarget.getSavingsType());
+        System.out.println("You have saved $" + savingsTarget.getCurrentSaved()
+        + " towards your goal. You still need to save $" + savingsTarget.remaingToSave(savingsTarget.getTargetAmount() - savingsTarget.getCurrentSaved())
+        + " to reach your goal.");
+    }
+
 }
 
